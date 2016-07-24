22e882f3757bfbcaec08882f5301284da56ff052
Mark Daugherty <mark.r.daugherty@gmail.com>
2016-07-24 18:27:45 -0500

    update.

--- circuit-sling-models-core/src/main/groovy/com/icfolson/aem/circuit/models/components/content/Breadcrumb.groovy
+++ circuit-sling-models-core/src/main/groovy/com/icfolson/aem/circuit/models/components/content/Breadcrumb.groovy
@@ -3,13 +3,14 @@ package com.icfolson.aem.circuit.models.components.content
 import com.citytechinc.cq.component.annotations.Component
 import com.day.cq.wcm.api.Page
 import com.icfolson.aem.library.core.constants.ComponentConstants
+import org.apache.sling.api.SlingHttpServletRequest
 import org.apache.sling.api.resource.Resource
 import org.apache.sling.models.annotations.Model
 
 import javax.inject.Inject
 
 @Component(value = "Breadcrumb", group = ComponentConstants.GROUP_HIDDEN, noDecoration = true)
-@Model(adaptables = Resource)
+@Model(adaptables = [Resource, SlingHttpServletRequest])
 class Breadcrumb {
 
     @Inject
--- circuit-sling-models-core/src/main/groovy/com/icfolson/aem/circuit/models/injectors/ComponentInjector.groovy
+++ circuit-sling-models-core/src/main/groovy/com/icfolson/aem/circuit/models/injectors/ComponentInjector.groovy
@@ -2,12 +2,6 @@ package com.icfolson.aem.circuit.models.injectors
 
 import com.day.cq.wcm.api.Page
 import com.day.cq.wcm.api.PageManager
-import com.day.cq.wcm.api.WCMMode
-import com.day.cq.wcm.api.components.ComponentContext
-import com.day.cq.wcm.api.components.EditContext
-import com.day.cq.wcm.api.designer.Design
-import com.day.cq.wcm.api.designer.Designer
-import com.day.cq.wcm.api.designer.Style
 import groovy.util.logging.Slf4j
 import org.apache.felix.scr.annotations.Component
 import org.apache.felix.scr.annotations.Property
@@ -16,7 +10,6 @@ import org.apache.sling.api.SlingHttpServletRequest
 import org.apache.sling.api.resource.Resource
 import org.apache.sling.api.resource.ResourceResolver
 import org.apache.sling.api.resource.ValueMap
-import org.apache.sling.api.scripting.SlingBindings
 import org.apache.sling.models.spi.DisposalCallbackRegistry
 import org.apache.sling.models.spi.Injector
 import org.osgi.framework.Constants
@@ -25,13 +18,6 @@ import javax.jcr.Node
 import javax.jcr.Session
 import java.lang.reflect.AnnotatedElement
 import java.lang.reflect.Type
-
-import static com.adobe.cq.sightly.WCMBindings.COMPONENT
-import static com.adobe.cq.sightly.WCMBindings.COMPONENT_CONTEXT
-import static com.adobe.cq.sightly.WCMBindings.CURRENT_DESIGN
-import static com.adobe.cq.sightly.WCMBindings.CURRENT_STYLE
-import static com.adobe.cq.sightly.WCMBindings.DESIGNER
-import static com.adobe.cq.sightly.WCMBindings.EDIT_CONTEXT
 
 /**
  * Injector for objects derived from the current component context.
@@ -42,12 +28,7 @@ import static com.adobe.cq.sightly.WCMBindings.EDIT_CONTEXT
 @Slf4j("LOG")
 class ComponentInjector implements Injector {
 
-    private static final def REQUEST_INJECTABLES = [SlingHttpServletRequest, WCMMode,
-                                                    com.day.cq.wcm.api.components.Component, ComponentContext,
-                                                    EditContext, Designer]
-
-    private static final def RESOURCE_INJECTABLES = [Resource, ResourceResolver, ValueMap, Node, Session, Page,
-                                                     PageManager]
+    private static final def INJECTABLES = [Resource, ResourceResolver, ValueMap, Node, Session, Page, PageManager]
 
     @Override
     String getName() {
@@ -62,45 +43,9 @@ class ComponentInjector implements Injector {
         if (type instanceof Class) {
             def clazz = type as Class
 
-            if (REQUEST_INJECTABLES.contains(clazz)) {
-                value = getValueForRequest(clazz, adaptable)
-            } else if (RESOURCE_INJECTABLES.contains(clazz)) {
+            if (INJECTABLES.contains(clazz)) {
                 value = getValueForResource(clazz, adaptable)
             }
-        }
-
-        value
-    }
-
-    private def getValueForRequest(Class clazz, Object adaptable) {
-        def request = getRequest(adaptable)
-
-        def value = null
-
-        if (request) {
-            def bindings = request.getAttribute(SlingBindings.name) as SlingBindings
-
-            if (clazz == SlingHttpServletRequest) {
-                value = request
-            } else if (clazz == WCMMode) {
-                value = WCMMode.fromRequest(request)
-            } else if (clazz == com.day.cq.wcm.api.components.Component) {
-                value = bindings[COMPONENT] as com.day.cq.wcm.api.components.Component
-            } else if (clazz == ComponentContext) {
-                value = bindings[COMPONENT_CONTEXT] as ComponentContext
-            } else if (clazz == EditContext) {
-                value = bindings[EDIT_CONTEXT] as EditContext
-            } else if (clazz == Designer) {
-                value = bindings[DESIGNER] as Designer
-            } else if (clazz == Design) {
-                value = bindings[CURRENT_DESIGN] as Design
-            } else if (clazz == Style) {
-                value = bindings[CURRENT_STYLE] as Style
-            } else {
-                LOG.debug("class = {} is not supported by this injector", clazz.name)
-            }
-
-            LOG.debug("injecting class = {} with instance = {}", clazz.name, value)
         }
 
         value
@@ -134,16 +79,6 @@ class ComponentInjector implements Injector {
         }
 
         value
-    }
-
-    private SlingHttpServletRequest getRequest(Object adaptable) {
-        def request = null
-
-        if (adaptable instanceof SlingHttpServletRequest) {
-            request = adaptable as SlingHttpServletRequest
-        }
-
-        request
     }
 
     private Resource getResource(Object adaptable) {
