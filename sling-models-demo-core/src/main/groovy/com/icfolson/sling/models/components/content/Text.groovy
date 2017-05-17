ed9b814467e1e87091271f7ef8758dc6eb7715e3
Mark Daugherty <mark.r.daugherty@gmail.com>
2017-05-16 23:19:56 -0500

    update.

--- sling-models-demo-core/src/main/groovy/com/icfolson/sling/models/components/content/Text.groovy
+++ sling-models-demo-core/src/main/groovy/com/icfolson/sling/models/components/content/Text.groovy
@@ -1,14 +1,19 @@
 package com.icfolson.sling.models.components.content
 
+import com.citytechinc.cq.component.annotations.Component
 import com.citytechinc.cq.component.annotations.DialogField
 import com.citytechinc.cq.component.annotations.widgets.TextArea
-import com.icfolson.sling.models.annotations.ExportableComponent
 import com.icfolson.sling.models.exporters.AudioExporter
+import org.apache.sling.api.resource.Resource
+import org.apache.sling.models.annotations.Exporter
+import org.apache.sling.models.annotations.Model
 import org.apache.sling.models.annotations.Optional
 
 import javax.inject.Inject
 
-@ExportableComponent(value = "Text", resourceType = Text.RESOURCE_TYPE)
+@Component(value = "Text")
+@Model(adaptables = Resource, resourceType = Text.RESOURCE_TYPE)
+@Exporter(name = AudioExporter.NAME, extensions = "mp3")
 class Text implements AudioExportable {
 
     public static final String RESOURCE_TYPE = "sling-models-demo/components/content/text"
@@ -21,5 +26,10 @@ class Text implements AudioExportable {
     @TextArea
     @Inject
     @Optional
-    String text
+    private String text
+
+    @Override
+    String getText() {
+        text
+    }
 }
